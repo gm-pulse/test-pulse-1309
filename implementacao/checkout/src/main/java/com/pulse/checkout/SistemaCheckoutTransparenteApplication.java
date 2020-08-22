d168bf9ce4cc588263b4e2ec7a8f30830f1206c8
Leilanne Borges <leilanne.borgess@gmail.com>
2020-08-22 11:29:55 -0300

    2 - Adição de main.class e application.properties

---
+++ implementacao/checkout/src/main/java/com/pulse/checkout/SistemaCheckoutTransparenteApplication.java
@@ -0,0 +1,11 @@
+package com.pulse.checkout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaCheckoutTransparenteApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaCheckoutTransparenteApplication.class, args);
+    }
+}
