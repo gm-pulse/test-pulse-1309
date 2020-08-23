264ff9dab4fe0838b7a588cee9d02934ed60c212
Leilanne Borges <leilanne.borgess@gmail.com>
2020-08-23 14:25:06 -0300

    8 - Criação de custom exceptions

---
+++ implementacao/checkout/src/main/java/com/pulse/checkout/exception/CheckoutCustomException.java
@@ -0,0 +1,7 @@
+package com.pulse.checkout.exception;
+
+public class CheckoutCustomException extends RuntimeException{
+    public CheckoutCustomException(String message){
+        super(message);
+    }
+}
